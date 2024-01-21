e8c2fae65f5405732c07ab5b1cb6423d7af87642
abdal <abdalmatin69@gmail.com>
2024-01-21 15:40:53 +0700

    abdal:blocing rendering in route atau di pisahkan

---
+++ Frontend/app/cari/[slug]/SectionrRepository.tsx
@@ -0,0 +1,16 @@
+async function getDatarepos(param: string) {
+    const res = await fetch(`https://api.github.com/users/${param}/repos,`)
+    await new Promise(r => setTimeout(r, 2000))
+    return res.json()
+}
+
+const RepoLIst = async ({slug}:any) => {
+    const dataRepos = await getDataRepos(slug)  
+    return <>
+      <p>List repository</p>
+      <div>
+        {JSON.stringify(datarepos)}
+      </div>
+    </>
+}
+export default Repolist
--- Frontend/app/cari/[slug]/page.tsx
+++ Frontend/app/cari/[slug]/page.tsx
@@ -1,32 +1,41 @@
 import React from 'react'
 
-async function getDataUser(param:string){
+async function getDataUser(param: string) {
   const res = await fetch(`https://api.github.com/users/${param}`)
   return res.json()
 }
 
 
-async function getDatarepos(param:string){
+async function getDatarepos(param: string) {
   const res = await fetch(`https://api.github.com/users/${param}/repos,`)
-  await new Promise (r=>setTimeout(r,2000))
+  await new Promise(r => setTimeout(r, 2000))
   return res.json()
+}
+const RepoLIst = async ({slug}:any) => {
+  const dataRepos = await getDataRepos(slug)  
+  return <>
+    <p>List repository</p>
+    <div>
+      {JSON.stringify(datarepos)}
+    </div>
+  </>
 }
 
 
-export default async function Detailcari({params}:{params:{slug:string}}) {
+export default async function Detailcari({ params }: { params: { slug: string } }) {
   const dataUser = await getDataUser(params.slug)
-  const dataRepos = await getDataRepos(params.slug) 
- return (
-    <div> 
+  
+  return (
+    <div>
       <p>Detail user : {params.slug}</p>
       <div>
         {JSON.stringify(dataUser)}
       </div>
-
-      <p>List repository</p>
-      <div>
-      {JSON.stringify(datarepos)}
-      </div>
+      <div style={{marginTop:"10px"}}>
+      <suspanse fallback={<div>mencoba menampilkan repository....</div>}>
+      <repoList slug={params.slug}/>
+      </suspanse>
+    </div>
     </div>
   )
 }
